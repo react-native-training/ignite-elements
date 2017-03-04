49fc0e22cfe2652a58b323ede2c56ac8cfa2f751
Steve Kellock <steve@kellock.ca>
2017-03-04 12:14:57 -0500

    Fixes deprecations before launch.

--- plugin.js
+++ plugin.js
@@ -3,17 +3,15 @@
 
 const RNElements = 'react-native-elements'
 const EXAMPLE_FILE = 'ElementsExample.js'
-const PLUGIN_PATH = __dirname
-const APP_PATH = process.cwd()
 
 const add = async function (context) {
-  const { ignite, filesystem } = context
+  const { ignite } = context
 
   // install a npm module and link it
   await ignite.addModule(RNElements)
 
   // copy the component example file (if examples are turned on)
-  await ignite.addComponentExample(EXAMPLE_FILE, { title: 'Elements Example' })
+  await ignite.addPluginComponentExample(EXAMPLE_FILE, { title: 'Elements Example' })
 
   // Example of copying templates/Elements to App/Elements
   // if (!filesystem.exists(`${APP_PATH}/App/Elements`)) {
@@ -31,13 +29,13 @@ const add = async function (context) {
  * Remove yourself from the project.
  */
 const remove = async function (context) {
-  const { ignite, filesystem, patching } = context
+  const { ignite } = context
 
   // remove the npm module and unlink it
   await ignite.removeModule(RNElements)
 
   // remove the component example
-  await ignite.removeComponentExample(EXAMPLE_FILE)
+  await ignite.removePluginComponentExample(EXAMPLE_FILE)
 
   // Example of removing App/Elements folder
   // const removeElements = await context.prompt.confirm(
